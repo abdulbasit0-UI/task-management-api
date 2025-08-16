3895b8222c1d4f8680c4a15da638aebda12c5d2b
Abdulbasit <habdulbasit099@gmail.com>
2025-08-16 12:49:19 +0200

    feat(db): add TypeORM migrations, health check, and production-safe DB setup

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { ConfigService } from "@nestjs/config";
+import { DataSource } from "typeorm";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(configService.get<number>('PORT') ?? 3000);
+
+  const runMigrations = configService.get<boolean>('RUN_MIGRATIONS', false);
+  if (runMigrations) {
+    const dataSource = app.get(DataSource); // Use DataSource class as token
+    await dataSource.runMigrations();
+    console.log('Migrations have been run successfully!');
+  }
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
---
+++ src/migrations/1755340582904-CreateTasksTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTasksTable1755340582904 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
