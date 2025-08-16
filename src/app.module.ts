8ff3eab91e58cac137081c7c98f5fa6fcec7f0a4
Abdulbasit <habdulbasit099@gmail.com>
2025-08-16 12:10:17 +0200

    feat(common): add global exception filter, logggin, and configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,42 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TasksModule } from "./tasks/tasks.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import Joi from "joi";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "task_management_dev",
-      entities: [__dirname + "/**/*.entity{.ts,.js}"],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true, envFilePath: ".env", validationSchema: Joi.object({
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        LOG_LEVEL: Joi.string().valid('log', 'debug', 'verbose', 'warn', 'error').default('debug')
+      })
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'postgres'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DB_SYNC', true),
+        logging: configService.get<boolean>('DB_LOGGING', false),
+      })
     }),
     TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
+import { ConfigService } from "@nestjs/config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(configService.get<number>('PORT') ?? 3000);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
